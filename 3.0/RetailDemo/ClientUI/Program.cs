03dc07445d95338136104ffc42a7df4f530e2ffd
Robby Tarigan <robbytarigan@hotmail.com>
2019-04-25 22:17:45 +0100

    Created a message and a handler and used `SendLocal()` to send the message to the same endpoint.

--- 3.0/RetailDemo/ClientUI/Program.cs
+++ 3.0/RetailDemo/ClientUI/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Messages;
 using NServiceBus;
+using NServiceBus.Logging;
 
 namespace ClientUI
 {
     class Program
     {
+        static ILog log = LogManager.GetLogger<Program>();
+
+        static async Task RunLoop(IEndpointInstance endpointInstance) {
+            while (true) {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key) {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        
+                        await endpointInstance.SendLocal(command).ConfigureAwait(false);
+                        break;
+                    case ConsoleKey.Q:
+                        return;
+                    default:
+                    log.Info("Unknown input. Please try again");
+                    break;
+                }
+            }
+        }
+
         static async Task Main(string[] args)
         {
             Console.Title = "ClientUI";
@@ -16,8 +47,7 @@ namespace ClientUI
 
             var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
 
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
+            await RunLoop(endpointInstance).ConfigureAwait(false);
 
             await endpointInstance.Stop().ConfigureAwait(false);
         }
