4492b3685c6803b4c3c1e0aff768b4b992b5ded6
Robby Tarigan <robbytarigan@hotmail.com>
2019-04-29 22:32:36 +0100

    Throw an exception

--- 3.0/RetailDemo/Sales/PlaceOrderHandler.cs
+++ 3.0/RetailDemo/Sales/PlaceOrderHandler.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
@@ -7,13 +8,15 @@ using NServiceBus.Logging;
 namespace Sales {
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
-        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        private static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
         
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
 
             // This is where normally some business logic would occur
+
+            throw new Exception("BOOM");
             var orderPlaced = new OrderPlaced { OrderId = message.OrderId };
 
             return context.Publish(orderPlaced);
